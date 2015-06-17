63a09c24ead2f1597a3ae07fcf1468f5f21344ac
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-17 14:07:35 +0200

    cp chacks

--- manifests/add_tests.pp
+++ manifests/add_tests.pp
@@ -1,6 +1,9 @@
 define rspec_monitor::add_tests {
   file { "/usr/local/rspec_tests/${name}":
     ensure => directory,
+    recurse => remote,
+    recurselimit => 1,
+    souece => "puppet:///modules/${name}/spec/acceptance",
   }
 }
 
