8f0b5e6333babb4816d0d8c1c63ca0e5825b7e0a
Paul Gomersbach <p.gomersbach@rely.nl>
2015-06-23 11:14:50 +0200

    change to seperate file and dir resources

--- manifests/add_tests.pp
+++ manifests/add_tests.pp
@@ -1,9 +1,8 @@
 define rspec_monitor::add_tests {
   file { "/usr/local/rspec_tests/spec/${name}":
     ensure       => directory,
-    recurse      => remote,
-    recurselimit => 1,
-    links        => manage,
-    source       => "puppet:///modules/${name}/spec/acceptance",
+  }
+  file { "/usr/local/rspec_tests/spec/${name}/class_spec.rb":
+    source => "puppet:///modules/${name}/spec/acceptance/class_spec.rb",
   }
 }
