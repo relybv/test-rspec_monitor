3c9fc9c8d3bb0b031c5b9b7423c3d723c67e8cbe
Paul Gomersbach <p.gomersbach@rely.nl>
2016-06-08 09:11:26 +0000

    update to puppet 4

--- spec/classes/example_spec.rb
+++ spec/classes/example_spec.rb
@@ -14,8 +14,8 @@ describe 'rspec_monitor' do
           it { is_expected.to compile.with_all_deps }
  
           it { is_expected.to contain_class('rspec_monitor') }
-          it { is_expected.to contain_package('rspec-nagios-formatter').with_provider('gem') }
-          it { is_expected.to contain_package('serverspec').with_provider('gem') }
+          it { is_expected.to contain_package('rspec-nagios-formatter').with_provider('puppet_gem') }
+          it { is_expected.to contain_package('serverspec').with_provider('puppet_gem') }
           it { is_expected.to contain_file('/usr/local/rspec_tests/spec').with_ensure('directory') }
           it { is_expected.to contain_file('/usr/local/rspec_tests').with_ensure('directory') }
         end
